3bd513cd52839cbf15adbb2a306851e04f59ad67
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-04-02 08:27:32 +0900

    Omit value from range

--- main.go
+++ main.go
@@ -94,7 +94,7 @@ func run(args []string, outStream, errStream io.Writer) (exitCode int) {
 	}
 
 	var wg sync.WaitGroup
-	for url, _ := range cfg.URLs {
+	for url := range cfg.URLs {
 		wg.Add(1)
 		go fetch(url, errStream, &wg)
 	}
