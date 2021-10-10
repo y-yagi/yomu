7f478a6e2668407e0671a744b0cc2b919cc26047
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2021-10-11 08:01:21 +0900

    Add test for an invalid value

--- subscriber/subscriber_test.go
+++ subscriber/subscriber_test.go
@@ -37,3 +37,16 @@ func TestSubscribe_nofeeds(t *testing.T) {
 		t.Fatalf("expected \n%s\n\nbut got \n\n%s\n", want, got)
 	}
 }
+
+func TestSubscribe_invalidURL(t *testing.T) {
+	var cfg yomu.Config
+	cfgure := configure.Configure{Name: "yomu-test"}
+	subscriber := subscriber.NewSubscriber(cfg, cfgure)
+
+	err := subscriber.Subscribe("github.com/y-yagi/yomu")
+	got := err.Error()
+	want := `URI parser error: parse "github.com/y-yagi/yomu": invalid URI for request`
+	if got != want {
+		t.Fatalf("expected \n%s\n\nbut got \n\n%s\n", want, got)
+	}
+}
